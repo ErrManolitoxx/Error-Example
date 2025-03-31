650065eaa82d9846e9e796ecff0dc27cfa3b3ab8
Allan <allanglopez1312@gmail.com>
2025-03-31 10:12:28 +0200

    Ejercicio3. Está mal, tengo que hacerlo con un do while.

--- src/main/java/org/example/ExeptionsExercices.java
+++ src/main/java/org/example/ExeptionsExercices.java
@@ -11,11 +11,9 @@ public class ExeptionsExercices {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        StudentReader studentReader = new StudentReader();
-        var student = studentReader;
 
-        log.info(studentReader.toString());
-        readNumber();
+
+        readDoubleNumbers();
     }
 
     public static int readNumber() {
@@ -37,7 +35,38 @@ public class ExeptionsExercices {
 
     public static double readDoubleNumbers(){
 
+        double num1 = 1;
+        double num2;
 
+        try {
+            log.info("Introduzca un número: ");
+            num1 = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            log.error("El valor introducido no es un número. ");
+        } finally {
+            scanner.nextLine();
+        }
+
+        num2 = aDoubleMoreZero();
+
+        return num1 / num2;
+    }
+
+    public static double aDoubleMoreZero() {
+
+        double num = 1;
+            try{
+                log.info("Introduzca un número distinto de 0");
+                num = scanner.nextDouble();
+                if (num == 0) {
+                    log.info("El número es igual a cero, introduzca otro valor");
+                }
+            } catch (InputMismatchException e) {
+                log.error("El valor introducido no es un número.");
+            } finally {
+                scanner.nextLine();
+            }
+        return num;
     }
 
 
